bf094f0d2cd84478d07f0ab743359b091d194654
Maaran-17 <maaranrocks007@gmail.com>
2026-02-14 16:45:54 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -10,11 +10,11 @@
 
 // ============ ⚙ CONFIGURATION ============
 const config = {
-    host: 'my.7startv.me', // Replace with your Stalker-Portal host (e.g., 'example.com')
-    mac_address: '00:1A:79:F0:45:58', // Replace with your MAC address
-    serial_number: 'D8A5C6BC42CC6', // Replace with your serial number
-    device_id: '26CBEC7F9C6DC438B2657257B7C28FAFBF269DEB9921813F17315C6E279986FA', // Replace with your device_id
-    device_id_2: '26CBEC7F9C6DC438B2657257B7C28FAFBF269DEB9921813F17315C6E279986FA', // Replace with your device_id_2
+    host: 'http://geoiptv.one:8880/portal.php?type=stb', // Replace with your Stalker-Portal host (e.g., 'example.com')
+    mac_address: '00:1A:79:B2:EA:B3', // Replace with your MAC address
+    serial_number: '85D7176470706', // Replace with your serial number
+    device_id: 'EADFDE6F5A57421F9E3D11728951481821D98A9D5FB1D892EE6242263A2E194F', // Replace with your device_id
+    device_id_2: 'EADFDE6F5A57421F9E3D11728951481821D98A9D5FB1D892EE6242263A2E194F', // Replace with your device_id_2
     stb_type: 'MAG270', // Replace with Stalker-Portal Stb_type
     api_signature: '263', // No need to change
 };
